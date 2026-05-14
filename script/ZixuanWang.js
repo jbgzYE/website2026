d50fef11c1e9b2867d7d146869bd7a550397c8fb
煎饼果子 <jianbingguozi@ZixuandeMacBook-Pro.local>
2026-05-13 19:24:17 -0500

    1

---
+++ script/ZixuanWang.js
@@ -0,0 +1,5 @@
+// Add click listener to the text
+document.getElementById("zixuan-wang").addEventListener("click", function () {
+    // Navigate to About.html when the text is clicked
+    window.location.href = "About.html";
+  });
